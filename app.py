92dc83ffa45b0dfd75e5e9cf6f7b8bd95d1d043e
vikasmishra1711 <vm0734960@gmail.com>
2026-02-14 18:45:25 +0530

    Added Flask server for AWS deployment

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 print("trying to push code in aws")
 print("ci/cd checking")
 print("i think now it works")
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🚀 My CI/CD app is running on AWS EC2!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
